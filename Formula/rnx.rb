815dbe042a58d6754c3d151bb258f44184a3de84
Jay Ehsaniara <ehsaniara@gmail.com>
2025-08-15 06:26:57 +0000

    fix: brew install

--- Formula/rnx.rb
+++ Formula/rnx.rb
@@ -48,7 +48,7 @@ class Rnx < Formula
 
   # Dynamic URLs based on latest release
   arch_suffix = Hardware::CPU.intel? ? 'amd64' : 'arm64'
-  url "https://github.com/ehsaniara/joblet/releases/download/#{latest_version}/rnx-#{latest_version}-darwin-#{arch_suffix}.tar.gz"
+  url "https://github.com/ehsaniara/joblet/releases/download/#{latest_version}/rnx-v#{latest_version}-darwin-#{arch_suffix}.tar.gz"
 
   # Optional Node.js dependency for admin UI
   depends_on "node" => :optional
