ee264425bbdd814a65d9df87d85fd0e229a9dbf8
Jay Ehsaniara <ehsaniara@gmail.com>
2025-09-12 20:34:41 -0700

    fix ui server port

--- admin/ui/vite.config.ts
+++ admin/ui/vite.config.ts
@@ -17,11 +17,11 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:3001',
+                target: 'http://localhost:5173',
                 changeOrigin: true,
             },
             '/ws': {
-                target: 'ws://localhost:3001',
+                target: 'ws://localhost:5173',
                 ws: true,
             },
         },
